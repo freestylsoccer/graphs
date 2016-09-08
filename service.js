5ee5cc05ba7fac6844b201a93b86d79d582fde3e
freestylsoccer <freestylsoccer@gmail.com>
2016-09-07 22:53:39 -0500

    services.js

---
+++ service.js
@@ -0,0 +1,81 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+
+.factory('Chartfactory',function(){
+        var graphfac = {};
+        var graph = [
+           {
+             labels: ['January','february', 'march']
+           },
+           {
+             series: ['Series A', 'Series B']
+           },
+           {
+             datas:[
+               [65, 59, 80, 81, 56, 55, 40],
+               [28, 48, 40, 19, 86, 27, 90]
+             ]
+           }
+         ];
+ 
+
+  graphfac.getGraphs = function(){
+        return graph;
+      };
+      graphfac.getGraph = function(index){
+        return graph [index];
+      };
+  return graphfac;
+
+
+
+});
